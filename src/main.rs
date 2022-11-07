26bba9e1fb2b3f34f005f8ca8dc78ae145e1504b
Simon Simonsson <simonsson.simon@gmail.com>
2022-11-07 22:38:01 +0100

    decode `BlobHeader` from file
    but it does not work...

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use osm::proto::items;
+use prost::Message;
 use quick_xml;
 
-fn foo(fp: &str) {}
+fn foo(fp: &str) {
+    //let r = std::io::BufReader::new(std::fs::File::open(fp).unwrap());
+
+    let b = std::fs::read(fp).unwrap();
+
+    let x: &[u8] = &b;
+    //let mut buf = std::io::Cursor::new(x);
+
+    //let mut buf = bytes::Bytes::from(b);
+
+    let bh = items::BlobHeader::decode(x).unwrap();
+    println!("blob header: {:?}", bh);
+}
 
 fn read_xml(fp: &str) -> Result<(), quick_xml::de::DeError> {
     let _d = osm::xml::Data::from_reader(std::io::BufReader::new(std::fs::File::open(fp).unwrap()))
