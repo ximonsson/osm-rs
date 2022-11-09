5046b0f1c5797418bda1bb44514f35087d1a8fca
Simon Simonsson <simon.simonsson@posten.no>
2022-11-09 08:02:00 +0100

    fixed reading first `Blob`
    apparently you need to clear the buffer...

--- src/proto.rs
+++ src/proto.rs
@@ -6,25 +6,26 @@ pub mod items {
 }
 
 const BYTES_BLOB_HEADER_SIZE: u64 = 4;
-const MAX_BLOB_SIZE: usize = 32 * 2 ^ 20; // 32MB
+const MAX_BLOB_SIZE: usize = 2 ^ 25; // 32MB
 
-pub fn from_reader(mut r: impl std::io::Read) -> std::io::Result<()> {
+pub fn from_reader(mut r: impl std::io::Read + std::io::Seek) -> std::io::Result<()> {
     // create buffer
     let mut buf: Vec<u8> = Vec::with_capacity(MAX_BLOB_SIZE);
-    //let a: &mut [u8] = &mut buf;
 
     // read blob header size
     r.by_ref()
         .take(BYTES_BLOB_HEADER_SIZE)
         .read_to_end(&mut buf)?;
-    let bhs: u32 = u32::from_be_bytes(buf.as_ref().try_into().unwrap());
+    let bhs: u32 = u32::from_be_bytes(buf[..BYTES_BLOB_HEADER_SIZE as usize].try_into().unwrap());
 
     // decode blob header
-    r.by_ref().take(bhs as u64).read(&mut buf)?;
+    buf.clear();
+    r.by_ref().take(bhs as u64).read_to_end(&mut buf)?;
     let bh = items::BlobHeader::decode(buf.as_ref()).unwrap();
     println!("{:?}", bh);
 
     // decode blob
+    buf.clear();
     r.by_ref()
         .take(bh.datasize as u64)
         .read_to_end(&mut buf)
