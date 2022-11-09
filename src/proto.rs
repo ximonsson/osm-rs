67dec2b0e8f571e31d62afadc8449e2d2b3b0a74
Simon Simonsson <simon.simonsson@posten.no>
2022-11-09 09:40:55 +0100

    decode blob data
    now decoding and decompressing if necessary the data within the `Blob`
    message. need to solve how to deocde to different types, only supporting
    `HeaderBlock` at the moment.
    
    i can see that it works to step to next file block.

--- src/proto.rs
+++ src/proto.rs
@@ -1,36 +1,85 @@
+use flate2::{Decompress, FlushDecompress};
 use prost::Message;
-use std::io::Read;
+use std::io::{Read, Result};
 
 pub mod items {
     include!(concat!(env!("OUT_DIR"), "/osmpbf.rs"));
 }
 
+use items::blob::Data;
+use items::*;
+
 const BYTES_BLOB_HEADER_SIZE: u64 = 4;
 const MAX_BLOB_SIZE: usize = 2 ^ 25; // 32MB
 
-pub fn from_reader(mut r: impl std::io::Read + std::io::Seek) -> std::io::Result<()> {
-    // create buffer
-    let mut buf: Vec<u8> = Vec::with_capacity(MAX_BLOB_SIZE);
+fn read(r: impl Read, n: u64, mut buf: &mut Vec<u8>) -> Result<usize> {
+    buf.clear();
+    r.take(n).read_to_end(&mut buf)
+}
 
+fn read_blob_header(mut r: impl Read, mut buf: &mut Vec<u8>) -> Result<BlobHeader> {
     // read blob header size
-    r.by_ref()
-        .take(BYTES_BLOB_HEADER_SIZE)
-        .read_to_end(&mut buf)?;
+    read(r.by_ref(), BYTES_BLOB_HEADER_SIZE, &mut buf).unwrap();
     let bhs: u32 = u32::from_be_bytes(buf[..BYTES_BLOB_HEADER_SIZE as usize].try_into().unwrap());
 
     // decode blob header
-    buf.clear();
-    r.by_ref().take(bhs as u64).read_to_end(&mut buf)?;
-    let bh = items::BlobHeader::decode(buf.as_ref()).unwrap();
-    println!("{:?}", bh);
+    read(r.by_ref(), bhs as u64, &mut buf).unwrap();
+    let bh = BlobHeader::decode(buf.as_ref()).unwrap();
+    Ok(bh)
+}
 
-    // decode blob
-    buf.clear();
-    r.by_ref()
-        .take(bh.datasize as u64)
-        .read_to_end(&mut buf)
-        .unwrap();
-    let blob = items::Blob::decode(buf.as_ref()).unwrap();
+fn decode_blob(b: Blob, h: BlobHeader) {
+    let n: usize = match b.raw_size {
+        Some(x) => x as usize,
+        None => 0,
+    };
+
+    let mut buf = vec![0; n];
+    let status = match b.data {
+        Some(Data::ZlibData(x)) => Decompress::new(true)
+            .decompress(&x, &mut buf, FlushDecompress::Finish)
+            .unwrap(),
+        _ => todo!("support more"),
+    };
+    println!("{:?}", status);
+
+    let msg = match h.r#type.as_str() {
+        "OSMHeader" => Some(HeaderBlock::decode(buf.as_ref()).unwrap()),
+        _ => todo!(),
+        //"OSMData" => Some(PrimitiveBlock::decode(buf.as_ref()).unwrap()),
+    };
+    println!("{:?}", msg);
+}
+
+fn read_blob(mut r: impl Read, n: u64, mut buf: &mut Vec<u8>) -> Result<Blob> {
+    read(r.by_ref(), n, &mut buf).unwrap();
+    let blob = Blob::decode(buf.as_ref()).unwrap();
+    Ok(blob)
+}
+
+pub fn from_reader(mut r: impl Read) -> Result<()> {
+    // create buffer
+    let mut buf: Vec<u8> = Vec::with_capacity(MAX_BLOB_SIZE);
+
+    // read blob header
+    let mut header = read_blob_header(r.by_ref(), &mut buf).unwrap();
+    println!("{:?}", header);
+
+    // read blob
+    let mut blob = read_blob(r.by_ref(), header.datasize as u64, &mut buf).unwrap();
+    println!("{:?}", blob);
+
+    // decode the blob to correct
+    decode_blob(blob, header);
+
+    println!("----------------------------");
+
+    // read blob header
+    let mut header = read_blob_header(r.by_ref(), &mut buf).unwrap();
+    println!("{:?}", header);
+
+    // read blob
+    let mut blob = read_blob(r.by_ref(), header.datasize as u64, &mut buf).unwrap();
     println!("{:?}", blob);
 
     Ok(())
