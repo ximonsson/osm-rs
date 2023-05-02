1d4a6dc6e29c4be3b8bb0df839586924dbb14ff2
Simon Simonsson <simon.simonsson@posten.no>
2023-05-02 12:38:11 +0200

    Bit More Useful Maybe

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 use quick_xml;
 
-fn foo(fp: &str) -> std::io::Result<()> {
+fn foo(fp: &str) -> std::io::Result<osm::File> {
     let f = std::fs::File::open(fp).unwrap();
-    match osm::File::from_proto_reader(f) {
-        Err(e) => Err(e),
-        Ok(_) => Ok(()),
-    }
+    osm::File::from_proto_reader(f)
 }
 
-fn read_xml(fp: &str) -> Result<(), quick_xml::de::DeError> {
-    let _d =
-        osm::File::from_reader(std::io::BufReader::new(std::fs::File::open(fp).unwrap())).unwrap();
-    Ok(())
+fn read_xml(fp: &str) -> Result<osm::File, quick_xml::de::DeError> {
+    osm::File::from_reader(std::io::BufReader::new(std::fs::File::open(fp).unwrap()))
 }
 
 fn main() {
@@ -21,12 +16,14 @@ fn main() {
     }
 
     let ext = std::path::Path::new(&args[1]).extension();
-    match ext.unwrap().to_str() {
+    let data: osm::File = match ext.unwrap().to_str() {
         Some("pbf") => foo(&args[1]).unwrap(),
         Some("osm") | Some("xml") => read_xml(&args[1]).unwrap(),
-        Some(x) => println!("Unrecognized file type {}!", x),
+        Some(x) => panic!("Unrecognized file type {}!", x),
         None => panic!("what?"),
     };
+
+    println!("{:?}", data);
 
     println!("pronto!");
 }
